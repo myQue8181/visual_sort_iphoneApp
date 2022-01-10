4799ffa13c532e2a7d3b8df3760db1ae6c704acc
myQue8181 <myQue8181@gmail.com>
2022-01-10 23:28:51 +0900

    Initial Commit

---
+++ visualSortIphone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  visualSortIphone
+//
+//  Created by yasugon_mac on 2022/01/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ visualSortIphone/visualSortIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  visualSortIphoneApp.swift
+//  visualSortIphone
+//
+//  Created by yasugon_mac on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct visualSortIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
